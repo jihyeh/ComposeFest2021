61fc0e102168f66e7182d45f91579757a0a09e42
jihye <jihyeh919@gmail.com>
2021-11-11 00:28:13 +0900

    [Title] Complex custom layout - StaggerGrid 생성

--- week 2-1-Layouts in JetpackCompose/LayoutCodelab/app/src/main/java/com/jihyeh/layoutcodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutCodelab/app/src/main/java/com/jihyeh/layoutcodelab/MainActivity.kt
@@ -43,6 +43,55 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun StaggerGrid(
+    modifier: Modifier = Modifier,
+    rows: Int = 3,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+
+        val rowWidths = IntArray(rows) { 0 }
+        val rowHeights = IntArray(rows) { 0 }
+
+        val placeables = measurables.mapIndexed { index, measurable ->
+            val placeable = measurable.measure(constraints)
+            val row = index % rows
+            rowWidths[row] += placeable.width
+            rowHeights[row] = Math.max(rowHeights[row], placeable.height)
+            placeable
+        }
+
+        val width =
+        rowWidths.maxOrNull()?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth))
+            ?: constraints.minWidth
+
+        val height =
+            rowHeights.sumOf { it }.coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        val rowY = IntArray(rows) { 0 }
+        for (i in 1 until rows) {
+            rowY[i] = rowY[i-1] + rowHeights[i-1]
+        }
+
+        layout(width, height) {
+            val rowX = IntArray(rows) { 0 }
+
+            placeables.forEachIndexed { index, placeable ->
+                val row = index % rows
+                placeable.placeRelative(
+                    x = rowX[row],
+                    y = rowY[row]
+                )
+                rowX[row] += placeable.width
+            }
+        }
+    }
+}
+
+@Composable
 fun BodyContentMyOwn(modifier: Modifier = Modifier) {
     MyOwnColumn(modifier.padding(8.dp)) {
         Text("MyOwnColumn")
