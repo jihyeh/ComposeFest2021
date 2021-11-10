7191566b18c8cbd6529534cd5392e5e8bd63b7c2
jihye <jihyeh919@gmail.com>
2021-11-11 00:01:06 +0900

    [Title] Create your custom layout

--- week 2-1-Layouts in JetpackCompose/LayoutCodelab/app/src/main/java/com/jihyeh/layoutcodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutCodelab/app/src/main/java/com/jihyeh/layoutcodelab/MainActivity.kt
@@ -18,8 +18,13 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.jihyeh.layoutcodelab.ui.theme.LayoutCodelabTheme
@@ -31,9 +36,75 @@ class MainActivity : ComponentActivity() {
         setContent {
             LayoutCodelabTheme {
                 // A surface container using the 'background' color from the theme
-                LazyList()
+                BodyContentMyOwn()
             }
         }
+    }
+}
+
+@Composable
+fun BodyContentMyOwn(modifier: Modifier = Modifier) {
+    MyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically.")
+        Text("We've done it by hand!")
+    }
+}
+
+@Composable
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        val placeables = measurables.map { measurable ->
+            measurable.measure(constraints)
+        }
+
+        var yPosition = 0
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            placeables.forEach { placeable ->
+                placeable.placeRelative(0, yPosition)
+                yPosition += placeable.height
+            }
+        }
+    }
+
+}
+
+fun Modifier.firstBaselineToTop(firstBaselineToTop: Dp) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+            //배치되는 위치
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    LayoutCodelabTheme {
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    LayoutCodelabTheme {
+        Text("Hi there!", Modifier.padding(top = 32.dp))
     }
 }
 
